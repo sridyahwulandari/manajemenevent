3c82244b6f3c76da101cc7f1a0615c90579f4940
sridyahwulandari <sridyhwd@gmail.com>
2022-02-10 12:32:23 +0700

    Update 1

---
+++ resources/views/components/frontend/header.blade.php
@@ -0,0 +1,29 @@
+<header id="header" class="fixed-top d-flex align-items-center">
+    <div class="container d-flex justify-content-between">
+
+      <div class="logo">
+        <h1><a href="index.html"><span>Manajemen</span>Event</a></h1>
+        <!-- Uncomment below if you prefer to use an image logo -->
+        <!-- <a href="index.html"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
+      </div>
+
+      <nav id="navbar" class="navbar">
+        <ul>
+          <li><a class="nav-link scrollto active" href="#hero">Beranda</a></li>
+          <li class="dropdown"><a href="#"><span>Produk</span> <i class="bi bi-chevron-down"></i></a>
+            <ul>
+              <li><a href="#">Panen</a></li>
+              <li><a href="#">Pakan</a></li>
+              <li><a href="#">Benih</a></li>
+              <li><a href="#">Kebutuhan Rumah Makan</a></li>
+            </ul>
+          </li>
+          <li><a class="nav-link scrollto" href="#services">Riwayat</a></li>
+          <li><a class="nav-link scrollto" href="#portfolio">Informasi Jadwal</a></li>
+          <li><a class="nav-link scrollto" href="#team">Login</a></li>
+        </ul>
+        <i class="bi bi-list mobile-nav-toggle"></i>
+      </nav><!-- .navbar -->
+
+    </div>
+  </header>
